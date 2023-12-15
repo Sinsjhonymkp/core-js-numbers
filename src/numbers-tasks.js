f9d72336ca4d3f24c5a16af9432ee1fe4bcd753e
Sinsjhonymkp <tohaantoha96@gmail.com>
2023-12-15 22:15:52 +0300

    feat: +3 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -157,8 +157,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -178,8 +178,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const numWPow = num / 10 ** pow;
+  return Math.round(numWPow) * 10 ** pow;
 }
 
 /**
@@ -199,8 +200,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const rootOf = Math.sqrt(n);
+  for (let i = 2; i <= rootOf; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
