24a18e18e79ec35a1f455860c5a69595a47405b2
Sinsjhonymkp <tohaantoha96@gmail.com>
2023-12-15 11:57:11 +0300

    feat: 3 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -105,8 +105,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const cosA = (x1 * y1 * (x2 * y2)) / (Math.abs(Math.sqrt(x1 ** 2) * y1 ** 2) * Math.abs(Math.sqrt(x2 ** 2 * y2 ** 2)));
-  return Math.cos(cosA);
+  return (
+    (Math.acos(x1 * x2 + y1 * y2) / Math.sqrt(x1 ** 2 + y1 ** 2)) *
+    Math.sqrt(x2 ** 2 + y2 ** 2)
+  );
 }
 
 /**
@@ -122,8 +124,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -137,8 +139,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  const val = String(value);
+  return val;
 }
 
 /**
